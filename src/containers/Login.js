ae5faf20d996aa7ab6fb005e65a4cdb9bc77b8bc
Will Gerecke <wlgfour@gmail.com>
2019-10-31 01:43:48 -0400

    added .npmrc to automatically call npm install with --save. Added a config that can hold environment variables thatshould only be changed in one place if there is an update un url. Added alerts so that when the front end talks to the back end, response data is shown.

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {EmailInput, PasswordInput} from "../components/FormFields";
 import {Button} from "../components/Elements";
+import request from "request-promise";
+import config from "../config";
 
 class Login extends Component {
 	constructor(props) {
@@ -11,14 +13,33 @@ class Login extends Component {
     };
     this.handleLoginClick = this.handleLoginClick.bind(this);
   }
-	
+
   handleLoginClick(){
     let login = {
       'username': this.state.emailValue,
       'password': this.state.passwordValue
     };
-    if (login.username != '' && login.password != '') {
-        this.props.loginHandler();
+    if (login.username !== '' && login.password !== '') {
+        const options = {
+            method: 'GET',
+            uri: `${config.api.URL}/login?u=${login.username}&p=${login.password}`,
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true // Automatically parses the JSON string in the response
+        };
+        let self = this;  // because this about to change. It's a bad work around but it's 2 am
+        request(options)
+            .then(function (res) {
+                alert(`remove the loading animation. Found user with credentials: ${res.username} -- ${res.password}`);
+                self.props.loginHandler();
+            })
+            .catch(function (err) {
+                alert(`there was an error: ${err.status} -- ${err.message}`);
+            });
+        alert('Display a loading animation');
+    } else {
+        alert('Please enter a username and password');
     }
   }
 
