423947a545cbbee5c689e84a38a84256e8bc1eee
Dakota McInnis <dakotamcinnis1@gmail.com>
2019-11-18 17:30:30 -0500

    Add Endpoints Access for entity information and entity invoices by persona

--- src/models/backend.js
+++ src/models/backend.js
@@ -1,12 +1,4 @@
-// This has implementation to read/write data to backend
-// Ultimately: we are using the endpoints provided by the backend
-
-// -----------------------------
-
-// CREATE ENTITY? (IF WE DO REGISTRATION)
-// GET ENTITY INFO
-// GET INVOICE INFO
-// etc. (See backend endpoint documentation)
+/* Using endpoints provided by RequestToPay_BackEnd */
 
 import config from "../config";
 import request from "request-promise";
@@ -40,4 +32,80 @@ function performLogin(view, credentials, successfulLoginHandler) {
     }.bind(this), 2000); // Set Delay (to test the loading animation)
 }
 
-export {performLogin};
+/**
+ * Get the information related to the entity with username 'username'.
+ *
+ * @param username - The username of the entity
+ * @return String - The entity information
+ */
+function getEntityInfoByUsername(username) {
+    const options = {
+        method: 'GET',
+        uri: `${config.api.URL}/entityByName?user=${username}`,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
+    return performRequest(options);
+}
+
+/**
+ * Get the ID (for future endpoint calls) of the entity with username 'username'.
+ *
+ * @param username - The username of the entity
+ * @return String - The entity's ID
+ */
+function getEntityIdByUsername(username) {
+    let allInfo = this.getEntityInfoByUsername(username);
+    return allInfo["EID"];
+}
+
+/**
+ * Get the orders pertaining to the entity with ID 'entityID',
+ * where the entity is the 'persona' (customer, seller or driver).
+ *
+ * @param entityId -
+ * @param persona
+ * @return
+ */
+function getOrdersByEntityAndPersona(entityId, persona) {
+    const options = {
+        method: 'GET',
+        uri: `${config.api.URL}/entityOrdersUInvoiceUEntityByIdAndPersona?eid=${entityId}&pass=${persona}`,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
+    return performRequest(options);
+}
+
+export {performLogin,
+    getEntityInfoByUsername,
+    getEntityIdByUsername,
+    getOrdersByEntityAndPersona};
+
+/* HELPER METHODS */
+
+/**
+ * This performs a request with the provided options,
+ * without setting the state of a view to and from loading.
+ *
+ * @param options - The options of the request to an endpoint
+ * @returns {string} - The output of the request
+ */
+function performRequest(options) {
+    let result = "";
+    setTimeout(function() {
+        request(options)
+            .then(function (res) {
+                result = res;
+            })
+            .catch(function (err) {
+                result = "";
+                alert(`There was an error: ${err.status} -- ${err.message}`);
+            });
+    }.bind(this), 1000);
+    return result;
+}
