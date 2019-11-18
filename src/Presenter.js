e65031bf7bd16abbeab2e6e175e9ec02963bc6ac
dionysus_pc <dionysus.cho@gmail.com>
2019-11-17 21:36:53 -0500

    Added transitions.  Added Menu Bar. Added Home Page elements.

--- src/Presenter.js
+++ src/Presenter.js
@@ -11,10 +11,12 @@
 //     - update view data (to package up the information for the view)
 //     - then send the view data back to the view
 
-import React, { Component } from 'react';
-import App from './views/App';
-import Login from './views/Login';
-import Header from "./components/Header"
+import React, { Component } from 'react'
+import Login from './views/Login'
+import Menu from "./components/Menu"
+import OrderTypeMenu from "./views/OrderTypeMenu"
+import CardList from "./views/CardList"
+import './Presenter.css'
 
 // enum of views
 const VIEW = {
@@ -29,17 +31,27 @@ class Presenter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentView: VIEW.login,
+      currentView: VIEW.login,     // default: VIEW.login
       username: 'Not Logged In',
-      menuColor: 'transparent',
+      loggedIn: false,
+      menuColor: 'transparent',           // default: transparent
     };
+
+    // Pass Methods to Components
     this.loginHandler = this.loginHandler.bind(this);
+    this.setMenuColor = this.setMenuColor.bind(this);
+    this.transitionToHome = this.transitionToHome.bind(this);
+    this.transitionToBuyerList = this.transitionToBuyerList.bind(this);
+    this.logOut = this.logOut.bind(this);
   }
 
   // Log In Methods ----------------------------//
 
   loginHandler(username){
-    this.setState({username: username}); // TODO: replace with value from DB
+    this.setState({
+      username: username,
+      loggedIn: true,
+    }); // TODO: replace with value from DB
     this.transitionToHome();
   }
 
@@ -52,7 +64,6 @@ class Presenter extends Component {
   //// menuHide(){}
   //// payMenuShow(){}
   //// payMenuHide(){}
-  //// logOut(){}
 
   // Transitions -----------------------------//
 
@@ -65,26 +76,52 @@ class Presenter extends Component {
     this.setState({currentView: VIEW.home});
     this.setMenuColor('var(--RED)');
   }
-  // transitionToBuyerList(){}
-  //transitionToSellerList(){} // TODO: Later
-  // transitionToBuyerInvoice(ID){}
-  // transitionToSellerInvoice(ID){}  // TODO: Later
-  // transitionToPayID(ID){}  // TODO: Later
 
+  transitionToBuyerList(){
+    this.setState({currentView: VIEW.cardList});
+    this.setMenuColor('var(--ORANGE)');
+  }
+
+  logOut() {
+    this.transitionToLogin();
+    this.setState({
+      username: 'Not Logged In!',
+      loggedIn: false
+    });
+    // clear cache stuff
+  }
+
+  // transitionToSellerList(){}       // TODO: Later
+  // transitionToBuyerInvoice(ID){}   // TODO: Later
+  // transitionToSellerInvoice(ID){}  // TODO: Later
+  // transitionToPayID(ID){}          // TODO: Later
+
+  // Views and functions passed to views
   viewSwitch(view){
     switch(view){
       case "login":
-        return <Login loginHandler = {this.loginHandler} />;
+        return <Login
+                  loginHandler = {this.loginHandler}/>;
+
       case "home":
-        return <App username={this.state.username}/>;
+        return <OrderTypeMenu
+                  username={this.state.username}
+                  transitionToBuyerList={this.transitionToBuyerList}/>;
+
+      case "cardList": // TODO: Update according to CardList props parameters.
+        return <CardList
+                  username={'ID'}/>;
     }
   }
 
   // Rendering the appropriate Views
   render() {
     return (
-      <div id="control">
-        <Header menuColor={this.state.menuColor}/>
+      <div id="presenter_block">
+        <Menu menuColor = {this.state.menuColor}
+              logOut = {this.logOut}
+              showMenu = 'true'
+        />
         <div id="bg_container">
         </div>
         {this.viewSwitch(this.state.currentView)}
--- src/views/OrderTypeMenu.js
+++ src/views/OrderTypeMenu.js
@@ -13,3 +13,66 @@
 
 // DELIVERIES
 // some statuses...
+
+// TODO: https://github.com/alvarotrigo/react-fullpage
+
+import React, { Component } from 'react';
+import {Helmet} from "react-helmet";
+import TextWelcome from "../components/TextWelcome";
+
+/**
+ *
+ * The default react web page.
+ *
+ * @returns {*}
+ * @constructor
+ */
+
+class OrderTypeMenu extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      usernameValue: this.props.username, // retrieve from Control
+    };
+  }
+
+  render() {
+    return (
+      <div id="home_container">
+        {/* Helmet - load stylesheet based on current page */}
+        <Helmet>
+          <link rel="stylesheet" type="text/css" href="/css/OrderTypeMenu.css"/>
+        </Helmet>
+
+        <div className={"home_block"} style={{backgroundColor: 'var(--MID_RED)'}}>
+            <TextWelcome username={this.state.usernameValue}/>
+        </div>
+
+        <div className={"home_block"} style={{backgroundColor: 'var(--MID_ORANGE)'}}>
+          <div className={"type_block"}>
+            <h1>Buying.</h1>
+              <ul>
+                <li>Ready to Pay.</li>
+                <li><s>Waiting for Delivery.</s></li>
+                <li><s>Completed Orders.</s></li>
+              </ul>
+          </div>
+        </div>
+
+        <div className={"home_block"} style={{backgroundColor: 'var(--PINK)'}}>
+          <div className={"type_block"}>
+            <h1>Selling.</h1>
+              <ul>
+                <li><s>Incomplete Orders.</s></li>
+                <li><s>Completed Orders.</s></li>
+              </ul>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default OrderTypeMenu;
