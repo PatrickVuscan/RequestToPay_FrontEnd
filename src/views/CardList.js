272cc11996c129e1a122d2e660d0818cd4b33356
Dakota McInnis <dakotamcinnis1@gmail.com>
2019-11-19 21:43:50 -0500

    Changed CardList to only show one copy of the orders

--- src/views/CardList.js
+++ src/views/CardList.js
@@ -64,9 +64,6 @@ export class CardList extends Component {
                     Ready To Pay.
                 </div>
                 {childElements}
-                {childElements}
-                {childElements}
-                {childElements}
             </div>
         );
     }
